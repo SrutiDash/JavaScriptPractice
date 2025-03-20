99ce3ec2710e67761a49668cd9d1398bc2139e47
Sruti Dash <sruti.dash1203@gmail.com>
2025-03-20 13:48:23 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,21 @@
+//arrays
+
+//using constructors
+
+num = new Array(1,2,3,4,5)
+console.log(num);
+
+//for most parts
+
+fruits = ['apple', 55.4 , 'orange', false,  'banana', 'grapes', 34]
+console.log(fruits) //printing array
+console.log(fruits[4]) //printing index of an array
+console.log(typeof(fruits[5])) //print type of index of an array\
+fruits[7] = 'hello' //adding new index in the existing array
+console.log(fruits)
+console.log(fruits[7])
+fruits[3] = 33 //initialising inexing of an array
+//note :if i were using cosnt fruits i would not be able to initilie
+// it again as in is a constant
+console.log(fruits)
+console.log(fruits[3])
